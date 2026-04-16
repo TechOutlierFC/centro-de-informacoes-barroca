717217dbeb6175786af6af22539e42dfdfd7d0e9
Guilherme Rosa <gdrumondrosa@gmail.com>
2026-04-16 19:34:24 -0300

    refact: novo jogo

--- pages/home.py
+++ pages/home.py
@@ -121,4 +121,4 @@ with col_central:
         st.markdown("<div class='custom-box'>Logo não encontrada</div>", unsafe_allow_html=True)
 
 
-st.markdown("<div class='fixed-update'><em>Última atualização 13/04/2026</em></div>", unsafe_allow_html=True)
+st.markdown("<div class='fixed-update'><em>Última atualização 16/04/2026</em></div>", unsafe_allow_html=True)
